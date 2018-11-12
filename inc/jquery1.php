688f9e97ee7d70225d72023808a6b8cd51d41a93
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 16:45:34 +0100

    Update jquery1.php

--- inc/jquery1.php
+++ inc/jquery1.php
@@ -1,1 +1,8 @@
- 
+<!-- jQuery -->
+<script src="../../adminlte/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap 4 -->
+<script src="../../adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- AdminLTE App -->
+<script src="../../adminlte/dist/js/adminlte.min.js"></script>
