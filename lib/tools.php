8d8365f097d55e0d01181b4d5a20c4b7be0e8da7
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 12:52:34 +0100

    Update tools.php

--- lib/tools.php
+++ lib/tools.php
@@ -1,1 +1,23 @@
- tools
+<?php 
+
+    // ex: nav_li_item("fa fa-book", "title", "#")
+    function nav_li_item($icon, $title, $url) {
+        ?>
+
+        <li class="nav-item">
+            <a href="<?php echo $url; ?>" class="nav-link">
+                <i class="nav-icon <?php echo $icon; ?>"></i>
+                <p>
+                   <?php echo $title; ?>
+                </p>
+            </a>
+            <ul class="nav nav-treeview my-class-2">
+                <!-- sub item (0) -->
+                <!-- sub item (1) -->
+            </ul>
+        </li>
+
+        <?php
+    }
+
+?>
