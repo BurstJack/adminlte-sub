d5d9e741c70e558a52fd8ca411d8fb617b8ad8c7
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 19:01:30 +0100

    Create nav.php

---
+++ lib/nav.php
@@ -0,0 +1,95 @@
+<?php 
+
+    /******************************************************
+     - nav_li_header(icon, title, url, arr)
+     - nav_li_item(icon, title, url)
+     - nav_li_item_sub(icon, title, url)
+     - nav_li_item_bottom_border(icon, title, url, arr)
+     ******************************************************/
+
+     function nav_li_header($icon, $title, $url, $arr) {
+          ?>
+
+          <li class="nav-item" style= "border-bottom: 1px solid #6d6d6d; margin-bottom: 10px; font-size:130%;">
+            <div class="hover-item">
+            <a href="../" class="nav-link">
+              <i class="nav-icon fa fa-sitemap"></i>
+              <span class="brand-text font-weight-light">
+                  <b style="font-size:110%;">Private Chains </b>
+              </span>
+            </a>
+            </div>
+          </li>
+
+        <?php
+     }
+
+     function nav_li_item_bottom_border($icon, $title, $url, $arr) {
+        ?>
+
+        <li class="nav-item" style= "border-bottom: 1px solid #6d6d6d; margin-bottom: 10px;">
+            <a href="<?php echo $url; ?>" class="nav-link">
+                <i class="nav-icon <?php echo $icon; ?>"></i>
+                <p>
+                   <?php echo $title; ?>
+                   <?php if (is_array($arr)) { ?>
+                       <i class="right fa fa-angle-left"></i>
+                   <?php } ?>
+                </p>
+            </a>
+
+            <?php if (is_array($arr)) { ?>
+                <ul class="nav nav-treeview" style= "line-height:50%;">
+                    <?php foreach ($arr as $sub) { ?>
+                        <?php nav_li_item_sub($sub["icon"], $sub["title"], $sub["url"]); ?>
+                    <?php } ?>
+                </ul>
+            <?php } ?>
+
+        </li>
+
+        <?php
+    }
+
+     function nav_li_item($icon, $title, $url, $arr) {
+        ?>
+
+        <li class="nav-item">
+            <a href="<?php echo $url; ?>" class="nav-link">
+                <i class="nav-icon <?php echo $icon; ?>"></i>
+                <p>
+                   <?php echo $title; ?>
+                   <?php if (is_array($arr)) { ?>
+                       <i class="right fa fa-angle-left"></i>
+                   <?php } ?>
+                </p>
+            </a>
+
+            <?php if (is_array($arr)) { ?>
+                <ul class="nav nav-treeview" style= "line-height:50%;">
+                    <?php foreach ($arr as $sub) { ?>
+                        <?php nav_li_item_sub($sub["icon"], $sub["title"], $sub["url"]); ?>
+                    <?php } ?>
+                </ul>
+            <?php } ?>
+
+        </li>
+
+        <?php
+    }
+
+
+    function nav_li_item_sub($icon, $title, $url) {
+        ?>
+              <li class="nav-item">
+                  <a href="<?php echo $url; ?>" class="nav-link">
+                      <i class="<?php echo $icon; ?> my-class-1"></i>
+                      <p> <?php echo $title; ?> </p>
+                  </a>
+              </li>
+        <?php
+    }
+
+
+
+?>
