541a634adb9c1bc884dcada74fd71a6ecbb8e6b0
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 23:29:24 +0100

    Create content2.php

---
+++ inc/content2.php
@@ -0,0 +1,21 @@
+  <?php include_once "../lib/boxes.php"; ?>
+
+
+ <div class="content-wrapper">
+
+    <br>
+    <section class="content">
+      <div class="container-fluid">
+
+        <div class="row">
+            <?php info_box("0", "500000", "ABCD");  ?> 
+            <?php info_box("1", "500000", "ABCD");  ?> 
+            <?php info_box("2", "500000", "ABCD");  ?> 
+            <?php info_box("3", "500000", "ABCD");  ?> 
+            <?php info_box("4", "500000", "ABCD");  ?> 
+        </div>
+
+     </div>
+    </section>
+
+</div>
