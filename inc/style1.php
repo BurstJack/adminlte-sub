eee3589f9e50569e731fd2cd1c3a0caa14fbc212
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 16:28:10 +0100

    Update style1.php

--- inc/style1.php
+++ inc/style1.php
@@ -1,1 +1,8 @@
-
+     <style>
+         .hover-item:hover, .hover-item:link, .hover-item:visited, .hover-item:active {
+	        color: #d0d0d0 !important;
+          }
+         .hover-item a:hover, .hover-item a:link, .hover-item a:visited, .hover-item a:active {
+	        color: #d0d0d0 !important;
+          }
+     </style>
