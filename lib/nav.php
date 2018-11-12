a2200442826374fcc5d644cd21abe84d4628a4d0
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 16:16:15 +0100

    Update nav.php

--- lib/nav.php
+++ lib/nav.php
@@ -1,1 +1,39 @@
+<?php 
 
+    /***********************************
+     - nav_li_item(icon, title, irl)
+     - nav_li_item_sub(icon, title, url)
+     ***********************************/
+
+     function nav_li_item($icon, $title, $url) {
+     ?>
+        <li class="nav-item">
+            <a href="<?php echo $url; ?>" class="nav-link">
+                <i class="nav-icon <?php echo $icon; ?>"></i>
+                <p>
+                   <?php echo $title; ?>
+                </p>
+            </a>
+            <?php /****
+            <ul class="nav nav-treeview my-class-2">
+                <!-- nav_li_item_sub(icon, title, url) -->
+                <!-- nav_li_item_sub(icon, title, url) -->
+            </ul>
+            ****/ ?>
+        </li>
+        <?php
+    }
+
+    // ex: nav_li_item_sub("fa fa-angle-double-right", "title", "#")
+    function nav_li_item_sub($icon, $title, $url) {
+        ?>
+        <li class="nav-item">
+            <a href="<?php echo $url; ?>" class="nav-link">
+                <i class="<?php echo $icon; ?> my-class-1"></i>
+                <p><?php echo $title; ?></p>
+            </a>
+        </li>
+        <?php
+    }
+
+?>
