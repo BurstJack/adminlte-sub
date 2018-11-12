fc24727ce7f66e16e878b2d048c2e012f0bda62e
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 16:26:12 +0100

    Update sidebar1.php

--- inc/sidebar1.php
+++ inc/sidebar1.php
@@ -1,1 +1,10 @@
- 
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <?php include "style1.php"; ?>
+
+    <div class="sidebar">
+        <?php include "nav2.php"; ?>
+    </div>
+</aside>
+
+
+<?php include "burst1.php"; ?>
