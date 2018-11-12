edf4b84fc4a6e3951c5b64ab317cf251f62af171
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 20:47:10 +0100

    Create sidebar2.php

---
+++ inc/sidebar2.php
@@ -0,0 +1,7 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <?php include "style1.php"; ?>
+
+    <div class="sidebar">
+        <?php include "nav2.php"; ?>
+    </div>
+</aside>
