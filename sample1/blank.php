c7e6982e27809cddb8b8f1a9853a30ee41d67344
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 13:08:37 +0100

    Create blank.php

---
+++ sample1/blank.php
@@ -0,0 +1,14 @@
+<?php include "../inc/header1.php"; ?>
+
+<body class="hold-transition sidebar-mini">
+
+    <div class="wrapper">
+        <?php include "../inc/topbar4.php";  ?>
+        <?php include "../inc/sidebar1.php"; ?>
+        <?php /**include "../inc/content1a.php"; **/ ?>
+    </div>
+
+    <?php include "../inc/jquery.php"; ?>
+
+</body>
+</html>
