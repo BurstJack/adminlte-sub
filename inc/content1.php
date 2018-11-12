0f63db2a175775be204643dd5737e153333d9163
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 22:32:58 +0100

    Create content1.php

---
+++ inc/content1.php
@@ -0,0 +1,23 @@
+  <?php include_once "../lib/boxes.php"; ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+
+        <div class="row">
+            <?php small_box("0", "Header 0", "Text 0", "Details 0");  ?> 
+            <?php small_box("1", "Header 1", "Text 1", "Details 1");  ?> 
+            <?php small_box("2", "Header 2", "Text 2", "Details 2");  ?> 
+            <?php small_box("3", "Header 3", "Text 3", "Details 3");  ?> 
+            <?php small_box("4", "Header 4", "Text 4", "Details 4");  ?> 
+        </div>
+
+
+     </div>
+    </section>
