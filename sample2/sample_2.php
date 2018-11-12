0b7a087230e2666756dbd052849dec1fee2530fa
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 20:48:55 +0100

    Update sample_2.php

--- sample2/sample_2.php
+++ sample2/sample_2.php
@@ -1,1 +1,14 @@
- 
+ <?php include "../inc/header1.php"; ?>
+
+<body class="hold-transition sidebar-mini">
+
+    <div class="wrapper">
+        <?php include "../inc/topbar1.php";  ?>
+        <?php include "../inc/sidebar2.php"; ?>
+        <?php /**include "../inc/content1a.php"; **/ ?>
+    </div>
+
+    <?php include "../inc/jquery1.php"; ?>
+
+</body>
+</html>
