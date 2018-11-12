a7a23237cc3a94464c7ff220cd246bcc1da4ebc9
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 15:29:34 +0100

    Update nav.php

--- lib/nav.php
+++ lib/nav.php
@@ -8,7 +8,6 @@
     // ex: nav_li_item("fa fa-book", "title", "#")
     function nav_li_item($icon, $title, $url) {
         ?>
-
         <li class="nav-item">
             <a href="<?php echo $url; ?>" class="nav-link">
                 <i class="nav-icon <?php echo $icon; ?>"></i>
@@ -23,22 +22,19 @@
             </ul>
             ****/ ?>
         </li>
-
         <?php
     }
 
 
     // ex: nav_li_item("fa fa-book", "title", "#")
     function nav_li_item_sub($icon, $title, $url) {
-        ?>
-     
+        ?>   
         <li class="nav-item">
             <a href="#" class="nav-link">
                 <i class="fa fa-angle-double-right my-class-1"></i>
                 <p>Dashboard v1</p>
             </a>
-        </li>
-        
+        </li>       
         ?>
      }
 
