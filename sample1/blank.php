61108d4c65cb1a2a4a7bbe4135c43339cc140899
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-12 16:20:30 +0100

    Update blank.php

--- sample1/blank.php
+++ sample1/blank.php
@@ -3,12 +3,12 @@
 <body class="hold-transition sidebar-mini">
 
     <div class="wrapper">
-        <?php include "../inc/topbar4.php";  ?>
+        <?php include "../inc/topbar1.php";  ?>
         <?php include "../inc/sidebar1.php"; ?>
         <?php /**include "../inc/content1a.php"; **/ ?>
     </div>
 
-    <?php include "../inc/jquery.php"; ?>
+    <?php include "../inc/jquery1.php"; ?>
 
 </body>
 </html>
