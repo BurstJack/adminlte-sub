e1d390e6d4bdf96d97a6e88aca2af5a6f559922b
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 14:49:41 +0100

    Create table_header_dt1.php

---
+++ inc/table_header_dt1.php
@@ -0,0 +1,22 @@
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Chain ID</th>
+                <th>Ticker</th>
+                <th>CreatorRS</th>
+                <th>UnixTime</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th></th>
+                <th>Chain ID</th>
+                <th>Ticker</th>
+                <th>CreatorRS</th>
+                <th>UnixTime</th>
+                <th>Amount</th>
+            </tr>
+        </tfoot>
+    </table>
