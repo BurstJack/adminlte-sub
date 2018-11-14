52f9dcaba5eed2a19a3e558bb187f7aa1e4c2f65
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 14:53:03 +0100

    Create jquery_dt1.php

---
+++ inc/jquery_dt1.php
@@ -0,0 +1,21 @@
+<!-- jQuery -->
+<script src="../../adminlte/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap 4 -->
+<script src="../../adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables -->
+<script src="../../adminlte/plugins/datatables/jquery.dataTables.js"></script>
+<script src="../../adminlte/plugins/datatables/dataTables.bootstrap4.js"></script>
+
+<!-- SlimScroll -->
+<script src="../../adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+
+<!-- FastClick -->
+<script src="../../adminlte/plugins/fastclick/fastclick.js"></script>
+
+<!-- AdminLTE App -->
+<script src="../../adminlte/dist/js/adminlte.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="../../adminlte/dist/js/demo.js"></script>
