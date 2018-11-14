afb62dbe424feade887e9e1e2421d9644895c1af
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 14:40:22 +0100

    Create sample_3d.php

---
+++ sample3/sample_3d.php
@@ -0,0 +1,34 @@
+<?php include "../inc/header1.php";  ?>
+<?php include "../inc/header_dt1.php";  ?>
+
+
+<body class="hold-transition sidebar-mini">
+
+<div class="wrapper">
+    <?php include "../inc/sidebar1.php"; ?>
+
+    <div class="content-wrapper">
+        <section class="content">
+           <div class="row">
+               <div class="col-12">
+
+                  <div class="card">
+                     <div class="card-body">
+                         <?php include "../inc/topbar_dt1.php"; ?>
+                         <?php include "../inc/table_header_dt1.php";?>
+                     </div>
+                 </div>
+           
+             </div>
+         </div>
+      </section>
+    </div>
+ 
+</div>
+
+
+
+<?php include "../inc/data_dt1.php"; ?>
+<?php include "../inc/jquery_dt1.php"; ?>
+</body>
+</html>
