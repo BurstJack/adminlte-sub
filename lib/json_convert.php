563663797749d3d12d7ad6a8c1fbae86767a1545
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 21:17:12 +0100

    Update json_convert.php

--- lib/json_convert.php
+++ lib/json_convert.php
@@ -5,6 +5,23 @@
     
     function json_2arr($json) {
     }
+
+    // 1st line csv holds column names
+    function csv_2arr($csv) {
+        $res = array();
+        $temp = explode("\n", $csv);
+        
+        foreach ($temp as $k=>$v) {
+            if ($k==0) { $keys = explode(";", $v); }
+            else { 
+                $values = explode(";", $v); 
+                $combine = array_combine($keys, $values);
+                $res[] = $combine;
+            }
+        }
+        
+        return $res;
+    }
     
     
 
