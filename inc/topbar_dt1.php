d33264249596cadb62fff4a51b441659bfead1a9
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 14:48:27 +0100

    Update topbar_dt1.php

--- inc/topbar_dt1.php
+++ inc/topbar_dt1.php
@@ -3,8 +3,7 @@
             <!-- small box -->
             <div class="small-box 
               <?php if (isset($color)) { echo $color; } else { echo "bg-info"; } ?>" 
-              style="margin: 0 -10px;"
-            >
+              style="margin: 0 -10px;">
               <div class="inner" style="padding-left: 10px; padding-right: 10px;">
                   <center><h4>Chains Genesis</h4></center>
               </div>
