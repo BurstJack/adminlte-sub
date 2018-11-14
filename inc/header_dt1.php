edd6062f3fb565bfbdff6a42b5487bdcd8462cb0
BurstJack <44145917+BurstJack@users.noreply.github.com>
2018-11-14 14:43:49 +0100

    Update header_dt1.php

--- inc/header_dt1.php
+++ inc/header_dt1.php
@@ -1,1 +1,28 @@
-
+<head>
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../../adminlte/plugins/datatables/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <style>
+        td.details-control {
+            background: url('https://datatables.net/examples/resources/details_open.png') no-repeat center center;
+            cursor: pointer;
+        }
+        tr.shown td.details-control {
+            background: url('https://datatables.net/examples/resources/details_close.png') no-repeat center center;
+        }
+        .dataTables_wrapper {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 12px;
+        }
+        .dataTables_wrapper .dataTables_paginate .paginate_button {
+            padding : 0px;
+            margin-left: 0px;
+            display: inline;
+            border: 0px;
+        }
+        .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+            border: 0px;
+        }
+    </style>
+</head>
